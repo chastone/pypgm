5075531599249f6e94b9ec350f8924628772c4c1
chastone <yi.luo.chasedream@gmail.com>
2015-03-23 00:41:48 -0400

    Create layers.py

---
+++ dl/layers.py
@@ -0,0 +1,41 @@
+import math_exp as me
+
+class ConvLayer(object):
+    def __init__(self, patch):
+        # patch is the conv filter
+        self.patch = patch
+
+    def output(img): # do I have to use output(self, img)
+        #me.conv(img, ndim = 2)
+
+    # the temporary convolutional operation 
+    def conv(self. input_imgs, nfeatures):
+
+        ## input_imgs:
+        #   -- In fact, it is a Tensor
+
+        img_dim         = input_imgs.shape[0]
+        img_channels    = input_imgs.shape[1]
+        nimgs           = input_imgs.shape[2]
+
+        ##
+
+        for i in xrange(nimgs) :
+            for j in 
+        
+        
+
+class DropOutLayer(object):
+
+
+
+class MaxPoolingLayer(object):
+
+
+class MeanPoolingLayer(object):
+
+class ReLULayer(object):
+
+
+
+
